d0e37b307fa30e682f98b616ca4431e1b716dd6b
Thisita <Thisita@users.noreply.github.com>
2017-02-09 20:57:14 -0500

    add base of card controller

--- public/core.js
+++ public/core.js
@@ -3,3 +3,9 @@ var bootstrap = require('bootstrap');
 var angular = require('angular');
 
 var app = angular.module('affectionatefeline', []);
+
+app.controller('cardCtrl', function($scope, $http) {
+	$http.get('cards.json').then(function(response) {
+		$scope.card = response.data[$scope.cardIndex];
+	});
+});
